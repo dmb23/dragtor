662960c12eae4d6e06fc56cbb9d8379dea3e2c16
Mischa Ungermann <mischa@dataworkz.de>
2024-08-09 11:11:31 +0200

    finish script for minimal local RAG

--- dragtor/script.py
+++ dragtor/script.py
@@ -1,8 +1,11 @@
-import requests
-from pathlib import Path
 import os
-from llama_cpp import Llama
+import sys
+from pathlib import Path
+
 import chromadb
+import requests
+from llama_cpp import Llama
+
 
 def _load_env():
     env_path = Path('../.env').resolve()
@@ -20,13 +23,14 @@ def load_jina_reader(addr: str) -> requests.Response:
 
 if __name__ == "__main__":
     addr = "https://www.hoopersbeta.com/library/a2-pulley-manual-for-climbers"
-    model_path = "../models/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf"
-    data_file = Path("../data/blog_1.md")
-    db_file = Path("../data/chroma.db")
+    model_path = "/Users/mischa/Projects/local/dragtor/models/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf"
+    data_file = Path("/Users/mischa/Projects/local/dragtor/data/blog_1.md")
+    db_file = Path("/Users/mischa/Projects/local/dragtor/data/chroma.db")
+    max_tokens=400
+
     llm = Llama(
         model_path,
         n_gpu_layers=15,
-        embedding=True,
     )
 
     # cache JINA API call to file
@@ -37,17 +41,50 @@ if __name__ == "__main__":
         data_file.write_text(full_text)
     else:
         full_text = data_file.read_text()
+    print("SCRIPT -- loaded data")
 
 
-    chunks = full_text.split("\n\n")
-    ids = [f"id{i}" for i in range(len(chunks))]
-    client = chromadb.PersistentClient(str(db_file.resolve()))
-    collection = client.create_collection(name="blog_1")
-    collection.add(documents=chunks, ids=ids)
+    # cache Embeddings & Vector Store
+    client = chromadb.PersistentClient(path=str(db_file.resolve()))
+    try:
+        collection = client.get_collection(name="blog_1")
+    except ValueError:
+        collection = client.create_collection(name="blog_1")
+        chunks = full_text.split("\n\n")
+        ids = [f"id{i}" for i in range(len(chunks))]
+        collection.add(documents=chunks, ids=ids)
+    print("SCRIPT -- created VS")
 
+    question = "What should I do with an A2 pulley injury?"
     results = collection.query(
-             query_texts=["What should I do with an A2 pulley injury?"],
+             query_texts=[question],
              n_results=5
             )
 
+    prompt = """Please use the following pieces of context to answer the question.
+    context:
+        {}
 
+    question:
+        {}
+
+    answer:
+    """.format(
+            # '\n'.join(results['documents'][0]),
+            results['documents'][0][0],
+            question)
+    print(f"SCRIPT -- prompt generated")
+
+    result = llm(
+             prompt,
+             max_tokens=max_tokens,
+            )
+    print(result['choices'][0]['text'])
+#    for generated in llm(
+#            prompt,
+#            max_tokens=max_tokens,
+#            echo=True,
+#            stream=True,
+#            ):
+#        print(generated['choices'][0]['text'], end="")
+#        sys.stdout.flush()
