ce891f4fb06a67e2a52f85589919414ada73a487
Mischa Ungermann <mischa@dataworkz.de>
2024-11-27 16:57:16 +0100

    initial implementation of late chunking index

--- src/dragtor/index/late_chunking.py
+++ src/dragtor/index/late_chunking.py
@@ -2,11 +2,19 @@ from dataclasses import dataclass, field
 from pathlib import Path
 
 import chromadb.api
+from loguru import logger
 import numpy as np
+import torch
+import torch.nn.functional as F
 from transformers import AutoModel, AutoTokenizer, PreTrainedModel, PreTrainedTokenizer
 
 from dragtor.config import conf
+from dragtor.index import RetrievalError
 from dragtor.index.chunk import Chunker, get_chunker
+from dragtor.index.rerank import Reranker, get_reranker
+from dragtor.utils import ident
+
+CHUNK_COLLECTION_NAME = "chunks"
 
 
 @dataclass
@@ -39,6 +47,7 @@ class LateChunkingIndex:
     tokenizer: PreTrainedTokenizer = field(init=False)
     embedding_model: PreTrainedModel = field(init=False)
     chunker: Chunker = field(default_factory=get_chunker)
+    reranker: Reranker = field(default_factory=get_reranker)
 
     def __post_init__(self):
         self._db_path = str(Path(conf.base_path) / conf.store.chromadb.path)
@@ -52,16 +61,31 @@ class LateChunkingIndex:
             "jinaai/jina-embeddings-v3", trust_remote_code=True
         )
 
-    def _tokenize_and_map_boundaries(self, text: str, chunk_annotations: list[tuple[int, int]], offsets: np.ndarray):
+    def collection(self, name: str):
+        return self.client.get_or_create_collection(
+            name=name,
+            metadata={
+                "hnsw:space": self._distance,
+            },
+        )
+
+    def _map_chunks_to_tokens(
+        self, chunk_annotations: list[tuple[int, int]], token_offsets: np.ndarray
+    ) -> list[tuple[int, int]]:
+        """Translate chunk boundaries from text to token space"""
         # Convert chunk annotations to NumPy arrays
-        chunk_annotations = np.array(chunk_annotations)
+        ann = np.array(chunk_annotations)
 
         # Find start token indices
-        start_conditions = (offsets[:, 0][:, np.newaxis] <= chunk_annotations[:, 0]) & (offsets[:, 1][:, np.newaxis] > chunk_annotations[:, 0])
+        start_conditions = (token_offsets[:, 0][:, np.newaxis] <= ann[:, 0]) & (
+            token_offsets[:, 1][:, np.newaxis] > ann[:, 0]
+        )
         start_tokens = np.argmax(start_conditions, axis=0)
 
         # Find end token indices
-        end_conditions = (offsets[:, 0][:, np.newaxis] < chunk_annotations[:, 1]) & (offsets[:, 1][:, np.newaxis] >= chunk_annotations[:, 1])
+        end_conditions = (token_offsets[:, 0][:, np.newaxis] < ann[:, 1]) & (
+            token_offsets[:, 1][:, np.newaxis] >= ann[:, 1]
+        )
         end_tokens = np.argmax(end_conditions, axis=0) + 1  # end_token is exclusive
 
         # Check for any unmapped boundaries
@@ -73,95 +97,80 @@ class LateChunkingIndex:
         token_chunk_annotations = list(zip(start_tokens, end_tokens))
         return token_chunk_annotations
 
-    # def _calculate_late_embeddings_v2(self, input_text: str):
-    #     # TODO: check the logic around a max length
-    #     # what happens when I want to embed a text that is longer than my embedding model can handle?
-    #     chunks, _ = self.chunker.chunk_and_annotate(input_text)
-    #
-    #     inputs = self.tokenizer(input_text, return_tensors="pt")
-    #     chunk_tokens = [
-    #         self.tokenizer(chunk, add_special_tokens=False, return_tensors="pt")["input_ids"]
-    #         for chunk in chunks
-    #     ]
-    #     if not (inputs["input_ids"][:, 1:-1] == torch.cat(chunk_tokens, 1)).all():
-    #         raise ValueError("Something is fishy with text / chunk embeddings!")
-    #     i_start = 1
-    #     chunk_token_annotations = []
-    #     for chunk in chunk_tokens:
-    #         i_end = i_start + chunk.shape[1]
-    #         chunk_token_annotations.append((i_start, i_end))
-    #         i_start = i_end
-    #
-    #     model_output = self.model(**inputs)
-    #     token_embeddings = model_output["last_hidden_state"].squeeze(0)
-    #     pooled_embeddings = [
-    #         token_embeddings[start:end].sum(dim=0) / (end - start)
-    #         for start, end in chunk_token_annotations
-    #     ]
-    #     pooled_embeddings = [embedding.detach().cpu().tolist() for embedding in pooled_embeddings]
-    #
-    #     return pooled_embeddings
+    def _calculate_late_embeddings(
+        self, input_text: str, chunk_annotations: list[tuple[int, int]]
+    ) -> list[list[float]]:
+        # TODO: this will break if the input_text is longer than the capacity of the embedding model
+        task = "retrieval.passage"
+        task_id = self.embedding_model._adaptation_map[task]
+        task_prefix = self.embedding_model._task_instructions[task]
 
-    # def index_texts(self, texts: list[str]) -> None:
-    #     all_chunks, all_ids, all_embeddings = [], [], []
-    #     for text in texts:
-    #         chunks, _ = self.chunker.chunk_and_annotate(text)
-    #         embeddings = self._calculate_late_embeddings_v2(text)
-    #         ids = [ident(chunk) for chunk in chunks]
-    #
-    #         all_chunks.extend(chunks)
-    #         all_ids.extend(ids)
-    #         all_embeddings.extend(embeddings)
-    #
-    # self.store.collection.add(documents=all_chunks, ids=all_ids, embeddings=all_embeddings)
+        inputs = self.tokenizer(
+            task_prefix + input_text, return_tensors="pt", return_offsets_mapping=True
+        )
 
-    # def query(self, question: str) -> list[str]:
-    #     n_results = conf.select("store.n_query", default=5)
-    #     n_ranked = conf.select("reranker.n_ranked", default=3)
-    #     _do_rerank = True
-    #     if n_results <= n_ranked:
-    #         n_ranked = n_results
-    #         _do_rerank = False
-    #
-    #     logger.debug(
-    #         f"extracting {n_results} documents from the store, then rerank down to {n_ranked}"
-    #     )
-    #     q_embedding = self.model.encode(question)
-    #     search_result = self.store.collection.query(
-    #         query_embeddings=[q_embedding], n_results=n_results
-    #     )
-    #     try:
-    #         found_docs = search_result["documents"][0]
-    #         logger.debug(f"obtained {len(found_docs)} documents")
-    #     except TypeError:
-    #         raise RetrievalError("Could not find any document in the index!")
-    #
-    #     if _do_rerank:
-    #         ranked_docs = self.reranker.rerank(question, found_docs, n_ranked)
-    #         logger.debug(f"obtained {len(ranked_docs)} after reranking")
-    #         return ranked_docs
-    #     return found_docs
+        # only single string `input_text`
+        adapter_mask = torch.full((1,), task_id, dtype=torch.int32)
+        with torch.no_grad():
+            model_output = self.embedding_model(**inputs, adapter_mask=adapter_mask)
 
-    # def _calculate_late_embeddings(
-    #     self, input_text: str, token_chunk_annotations: list[tuple[int, int]], max_length: int = -1
-    # ) -> list:
-    #     inputs = self.tokenizer(input_text, return_tensors="pt")
-    #     model_output = self.model(**inputs)
-    #
-    #     token_embeddings = model_output["last_hidden_state"]
-    #     outputs = []
-    #     if max_length > 0:  # remove annotations which go bejond the max-length of the model
-    #         token_chunk_annotations = [
-    #             (start, min(end, max_length - 1))
-    #             for (start, end) in token_chunk_annotations
-    #             if start < (max_length - 1)
-    #         ]
-    #     pooled_embeddings = [
-    #         token_embeddings[start:end].sum(dim=0) / (end - start)
-    #         for start, end in token_chunk_annotations
-    #         if (end - start) >= 1
-    #     ]
-    #     pooled_embeddings = [embedding.detach().cpu().numpy() for embedding in pooled_embeddings]
-    #     outputs.append(pooled_embeddings)
-    #
-    #     return outputs
+        token_offsets = inputs["offset_mapping"].squeeze(0).numpy()
+        token_chunk_annotations = self._map_chunks_to_tokens(chunk_annotations, token_offsets)
+
+        token_embeddings = model_output["last_hidden_state"].squeeze(0).float()
+
+        pooled_embeddings = [
+            token_embeddings[start:end].sum(dim=0) / (end - start)
+            for start, end in token_chunk_annotations
+        ]
+        pooled_embeddings = [
+            F.normalize(embedding, p=2, dim=0).detach().cpu().tolist()
+            for embedding in pooled_embeddings
+        ]
+
+        return pooled_embeddings
+
+    def index_texts(self, texts: list[str], metadata: list[dict] | None = None) -> None:
+        all_chunks, all_ids, all_embeddings = [], [], []
+        for text in texts:
+            chunks, chunk_annotations = self.chunker.chunk_and_annotate(text)
+
+            embeddings = self._calculate_late_embeddings(text, chunk_annotations)
+
+            ids = [ident(chunk) for chunk in chunks]
+
+            all_chunks.extend(chunks)
+            all_ids.extend(ids)
+            all_embeddings.extend(embeddings)
+
+        # Store the chunks, ids, embeddings, and token_chunk_annotations in the vector store
+        self.collection(CHUNK_COLLECTION_NAME).add(
+            documents=all_chunks,
+            ids=all_ids,
+            embeddings=all_embeddings,
+        )
+
+    def query(self, question: str) -> list[str]:
+        task = "retrieval.query"
+        n_results = conf.select("store.n_query", default=5)
+        n_ranked = conf.select("reranker.n_ranked", default=3)
+        _do_rerank = True
+        if n_results <= n_ranked:
+            n_ranked = n_results
+            _do_rerank = False
+
+        q_embedding = self.embedding_model.encode(question, task=task)
+        search_result = self.collection(CHUNK_COLLECTION_NAME).query(
+            query_embeddings=[q_embedding], n_results=n_results
+        )
+        try:
+            found_docs = search_result["documents"][0]
+            logger.debug(f"extracted {len(found_docs)} documents")
+        except TypeError:
+            raise RetrievalError("Could not find any document in the index!")
+
+        if _do_rerank:
+            ranked_docs = self.reranker.rerank(question, found_docs, n_ranked)
+            logger.debug(f"reranked down to {len(ranked_docs)} documents")
+            return ranked_docs
+        return found_docs
